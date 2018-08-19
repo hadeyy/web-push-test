598a48486d2811ae1c6f5ac26bd82acc026ae4a5
hadeyy <evita.sivakova@gmail.com>
2018-08-19 20:46:21 +0300

    v1.06

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/5.3.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.3.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.0/firebase-messaging.js');
 
 var config = {
     messagingSenderId: "705688537739"
