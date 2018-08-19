bf9d94195018f98343b2b107f1cbc5f8d6335544
hadeyy <evita.sivakova@gmail.com>
2018-08-19 21:46:07 +0300

    v1.14

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/5.3.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.3.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.0/firebase-messaging.js');
 
 var config = {
     apiKey: 'AIzaSyCc3480B8OlhNiT0XpsQt958_Pstm06Xyw',
